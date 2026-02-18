2349ffb25438e470a7d35dfc0bd72ac3253b4b34
localthunder <128249364+localthunder@users.noreply.github.com>
2026-02-18 16:58:49 +0000

    "Fix cookie parsing crash when userId cookie doesn't exist"

--- hablito/src/cookies/getUserIdFromCookie.jsx
+++ hablito/src/cookies/getUserIdFromCookie.jsx
@@ -2,10 +2,11 @@ export const getUserIdFromCookie = () => {
     // Check if the document object is available (running in a browser environment)
     if (typeof document !== 'undefined') {
       const cookieString = document.cookie;
-      const userIdCookie = cookieString
-        ? parseInt(cookieString.split('; ').find(row => row.startsWith('userId=')).split('=')[1], 10)
+      const userIdMatch = cookieString
+        ? cookieString.split('; ').find(row => row.startsWith('userId='))
         : null;
-  
+      const userIdCookie = userIdMatch ? parseInt(userIdMatch.split('=')[1], 10) : null;
+
       return userIdCookie;
     } else {
       console.error('Document object not available. Code is running in a non-browser environment.');
