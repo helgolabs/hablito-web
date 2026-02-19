b8ed275ab91bfca637059685ee534b2b99aa6272
localthunder <128249364+localthunder@users.noreply.github.com>
2026-02-19 16:23:50 +0000

    table package from remark-gfm

--- content/astro.config.mjs
+++ content/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
   site: 'https://conjugito.com',
-  integrations: [mdx()],
+  integrations: [mdx({ remarkPlugins: [remarkGfm] })],
 });
